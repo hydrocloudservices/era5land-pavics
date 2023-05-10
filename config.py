2d79aa448457e4b178430d4161d5b95cf62c0e19
sebastienlanglois <sebastien.langlois@polymtl.ca>
2023-05-10 18:26:42 -0400

    add new variables

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ from datetime import datetime, timedelta
 class Config(object):
 
     # Bucket configuration
-    BUCKET = 's3://era5/north-america/reanalysis/land/netcdf4'
+    #BUCKET = 's3://era5/north-america/reanalysis/land/netcdf4'
+    BUCKET = '/media/slanglois/ssd-2tb/era5/netcdf4'
     CLIENT_KWARGS = {'endpoint_url': 'https://s3.wasabisys.com',
                      'region_name': 'us-east-1'}
     CONFIG_KWARGS = {'max_pool_connections': 30}
@@ -16,14 +17,28 @@ class Config(object):
                        }
 
     # Dataset
-    START_DATE = "1950-01-01"
+    # START_DATE = "2023-01-01"
+    # END_DATE = (datetime.utcnow() - timedelta(days=6)).strftime('%Y-%m-%d')
+
+    # VARIABLES = {
+    #     'total_precipitation': 'tp',
+    #     '2m_temperature': 't2m',
+    #     'snow_depth_water_equivalent': 'sd',
+    # }
+
+    START_DATE = "1951-01-01"
     END_DATE = (datetime.utcnow() - timedelta(days=6)).strftime('%Y-%m-%d')
 
     VARIABLES = {
-        'total_precipitation': 'tp',
-        '2m_temperature': 't2m',
-        'snow_depth_water_equivalent': 'sd',
-    }
+        'snowfall': 'sf',
+        '10m_u_component_of_wind': 'u10',
+        '10m_v_component_of_wind': 'v10',
+        '2m_dewpoint_temperature': 'd2m',
+        'potential_evaporation': 'pev',
+        'surface_pressure': 'sp',
+        'surface_solar_radiation_downwards': 'ssrd',
+        'surface_thermal_radiation_downwards': 'strd'
+            }
 
     TIMES = ['00:00', '01:00', '02:00',
              '03:00', '04:00', '05:00',
